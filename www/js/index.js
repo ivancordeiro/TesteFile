1b92eed05ef1cdf20f4c89713bba2d55c216829f
ivancordeiro <ivancordeiro@yahoo.com.br>
2018-01-02 19:27:15 -0200

    '12'

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,60 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     console.log(cordova.file);
-alert('teste');
+    alert('teste');
+createFile();
+
+
+
+
+
+
+
+
+
+window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+
+    console.log('file system open: ' + fs.name);
+    fs.root.getFile("newPersistentFile.txt", { create: true, exclusive: false }, function (fileEntry) {
+
+        console.log("fileEntry is file?" + fileEntry.isFile.toString());
+        // fileEntry.name == 'someFile.txt'
+        // fileEntry.fullPath == '/someFile.txt'
+        writeFile(fileEntry, null);
+
+    }, onErrorCreateFile);
+
+}, onErrorLoadFs);
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+function createFile() {
+   var type = window.TEMPORARY;
+   var size = 5*1024*1024;
+   window.requestFileSystem(type, size, successCallback, errorCallback)
+
+   function successCallback(fs) {
+      fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
+         alert('File creation successfull!')
+      }, errorCallback);
+   }
+
+   function errorCallback(error) {
+      alert("ERROR: " + error.code)
+   }
+
+ alert('teste 2');
+	
 }
