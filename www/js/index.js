d986e40565ed277bb00b8c6c9d82341a279038ad
ivancordeiro <ivancordeiro@yahoo.com.br>
2018-01-02 14:30:39 -0200

    'atu4'

--- www/js/index.js
+++ www/js/index.js
@@ -62,7 +62,7 @@ cordova.InAppBrowser.open(encodeURI('http://www.ivanprogramador.com.br/teste/had
 var ft = new FileTransfer();
 ft.download(
   "http://www.ivanprogramador.com.br/teste/hadaya/cliente/teste.pdf", // what u download
-  "/sdcard/test.zip", // this is the filename as well complete url
+  "/sdcard/test.pdf", // this is the filename as well complete url
   // fileSystem.root.toURL() + "test.zip",  use ios and others
   function(entry) {
     alert("success");
