2f251b02453ead05de6814fe7da7f846ff7c3c09
ivancordeiro <ivancordeiro@yahoo.com.br>
2018-01-03 03:02:21 -0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,39 @@
 document.addEventListener("deviceready", onDeviceReady, false); 
 
 function onDeviceReady() { 
+alert('ok 1');
+	
+	var myPath = cordova.file.externalRootDirectory; 
+	// We can use the default externalRootDirectory or use a path : file://my/custom/folder
+	
+	var myPath2 = cordova.file.dataDirectory; 
+
+	alert('myPath:' + myPath);
+	alert('myPath2:' + myPath2);
+	
+	var ft = new FileTransfer();
+
+ft.download(
+  "http://www.ivanprogramador.com.br/teste/hadaya/cliente/teste.pdf", // what u download
+ // "/sdcard/test.pdf", // this is the filename as well complete url
+myPath + "arquivos/test5.pdf", // this is the filename as well complete url
+  // fileSystem.root.toURL() + "test.pdf",  use ios and others
+  function(entry) {
+    alert("success");
+    alert(JSON.stringify(entry));
+
+  },
+  function(err) {
+    alert(err);
+    alert(JSON.stringify(err));
+  }
+);
+	
+}
+
+
+/*
+function onDeviceReady() { 
 createFile();
 alert('ok 1');
 }
@@ -23,3 +56,4 @@ function createFile() {
    }
 	
 }
+*/
