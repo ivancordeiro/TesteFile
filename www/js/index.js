fc008c91956cd82c90567d22fbef4d22207b3005
ivancordeiro <ivancordeiro@yahoo.com.br>
2018-01-17 09:59:47 -0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -46,6 +46,12 @@ alert('chamou funcao dowload');
 
 
 function arq2(){
-cordova.InAppBrowser.open(encodeURI('http://www.ivanprogramador.com.br/teste/hadaya/cliente/teste.pdf'), '_blank', 'location=no,hidden=no');
+cordova.InAppBrowser.open(encodeURI('http://www.ivanprogramador.com.br/teste/hadaya/cliente/teste.pdf'), '_blank', 'location=yes,hidden=no');
+	   
+    }
+
+
+function arq3(){
+cordova.InAppBrowser.open(encodeURI('https://www.ivanprogramador.com.br/teste/hadaya/cliente/pedido_teste.php?id=121'), '_blank', 'location=yes,hidden=no');
 	   
     }
